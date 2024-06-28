340dcf3a707b408fe666a15a805ce3e68cd1ac5a
Charlie <charlyavellaneda@hotmail.com>
2024-06-28 02:22:15 -0500

    feat(project): fixing minor errors

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  serverBasePath: 'http://localhost:8080/api/v1'
+  serverBasePath: 'https://outstanding-amazement-production.up.railway.app/api/v1'
 }
